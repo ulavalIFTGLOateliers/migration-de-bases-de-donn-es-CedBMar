cf783c48b3889a5213267e47f2d651aeee1552eb
Cédric-B. Marsolais <cedric-b.marsolais.1@ulaval.ca>
2024-03-31 14:52:38 -0400

    Cedric B. Marsolais Final Commit

--- db_scripts/migrate_1.sql
+++ db_scripts/migrate_1.sql
@@ -9,6 +9,7 @@ VALUES ('Crazy Duo', 2015, 'rock'),
        ('Luna', 2009, 'classical'),
        ('Mysterio', 2019, 'pop');
 
+
 ALTER TABLE singer RENAME TO musician;
 ALTER TABLE musician RENAME COLUMN singerName TO musicianName;
 ALTER TABLE musician ADD COLUMN role VARCHAR(50);
